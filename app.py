504a8087714d5738b6837d5780e0b26e6d916d87
harisafriansyah <harisafriansyahnew@gmail.com>
2024-12-20 14:00:27 +0700

    cors

--- app.py
+++ app.py
@@ -40,14 +40,7 @@ def create_app():
     @app.route('/')
     def home():
         return {"message": "Welcome to Flask!"}
-    
-    @app.after_request
-    def after_request(response):
-        response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
-        response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-        response.headers["Access-Control-Allow-Credentials"] = "true"
-        return response
+
 
     return app
 
